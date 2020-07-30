9ae575767ce5c9309970700afa8b34df74f332e3
FutureMeng <futuremeng@gmail.com>
2020-07-30 08:20:56 +0800

    electron-vue-init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-vue-escpos-bridge'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
